bb159f7d2cd2db9ef1563468ce97d779a1b0b015
ngangu63 <banabalukala@gmail.com>
2026-06-30 19:33:58 -0700

    Dede anniversaire 2

--- src/app/features/lukala-samuse/lukala-samuse.component.ts
+++ src/app/features/lukala-samuse/lukala-samuse.component.ts
@@ -20,7 +20,8 @@ export class LukalaSamuseComponent {
 
 items: MediaItem[] = [
   
-   {type: 'video', src: 'assets/mp3/DedeAniversaire2026.mp4', placeholder: 'assets/images/DedeAniversaire2026.jpeg'},
+   {type: 'video', src: 'assets/mp3/DedeAniversaire2026.mp4', placeholder: 'assets/images/DedeAniversaire2026.jpg'},
+  
    
   
     
