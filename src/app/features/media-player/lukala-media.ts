40863a5a122f5604f136aa9b550da98a3b6a7687
ngangu63 <banabalukala@gmail.com>
2026-09-16 17:22:32 -0700

    remove bilanga11

--- src/app/features/media-player/lukala-media.ts
+++ src/app/features/media-player/lukala-media.ts
@@ -41,25 +41,4 @@ export const lukalaMediaList: MediaFile[] = [
     placeholder: 'assets/images/CantineModified1.jpg',
   },
 
-  {
-    name: 'Bilanga 11',
-    link: ['/media', 'bilanga11'],
-    source: 'assets/mp3/bilanga11.mp4',
-    type: 'mp4',
-    placeholder: 'assets/plantation/bilanga11.jpeg',
-  },
-  {
-    name: 'Bilanga 12',
-    link: ['/media', 'bilanga12'],
-    source: 'assets/mp3/bilanga12.mp4',
-    type: 'mp4',
-    placeholder: 'assets/plantation/bilanga12.jpg',
-  },
-  {
-    name: 'La bouture',
-    link: ['/media', 'bouture1'],
-    source: 'assets/mp3/bouture1.mp4',
-    type: 'mp4',
-    placeholder: 'assets/plantation/abouture1.jpeg',
-  },
 ];
